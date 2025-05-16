1e59cf77cde188c7d832a1114adf2fcbb44b870d
Gustavo <48855862@fatimarem.edu.ar>
2025-05-16 11:59:52 -0300

    Ejercicio15 arreglado

--- src/EvPrimerBimestre2/Ejercicio15.java
+++ src/EvPrimerBimestre2/Ejercicio15.java
@@ -7,7 +7,7 @@ public class Ejercicio15 {
 	public static void main(String[] args) {
 		
 		
-		char s = 's', m = 'm', l = 'l';
+		
 		String Talle;
 		
 		System.out.println("Ingrese su talle (en minuscula)");
